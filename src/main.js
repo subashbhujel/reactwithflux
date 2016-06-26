a2a520c03e3c81a75dcddd58a91686562b1bc955
Subash Bhujel <subasb@microsoft.com>
2016-06-26 12:16:32 -0700

    Using a react-routes for all routing needs

--- src/main.js
+++ src/main.js
@@ -1,34 +1,9 @@
-$ = jquery = require('jquery');
+"use strict";
+
 var React = require('react');
-var Home = require('./components/homePage');
-var Authors = require('./components/author/authorPage');
-var About = require('./components/about/aboutPage');
-var Header = require('./components/common/header');
+var Router= require('react-router');
+var routes = require('./routes');
 
-var App = React.createClass({
-	render : function(){
-		var child;
-
-		switch(this.props.route){
-			case 'about' : Child = About;break;
-			case 'authors': Child = Authors; break;
-			default: Child = Home;
-		}
-		return(
-			<div>
-				<Header />
-				<Child/>
-			</div>
-		);
-	}	
+Router.run(routes,function(Handler){
+	React.render(<Handler />, document.getElementById('app'));
 });
-
-function render(){
-	var route = window.location.hash.substr(1);
-	React.render(<App route = {route} />, document.getElementById('app'));
-}
-
-window.addEventListener('haschange',render);
-render();
-
-//React.render(<Home />, document.getElementById('app'));
