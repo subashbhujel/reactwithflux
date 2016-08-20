61b683b5b1eb262c32b16f000452448b38b593af
subashbhujel <kathmandu1>
2016-08-20 14:34:07 -0700

    Adding transition to and from statics

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,6 +3,23 @@
 var React = require('react');
 
 var About = React.createClass({	
+	statics: {
+		// Triggered when users tries to come to this page.
+		willTransitionTo: function (transition,params,query,callback) {
+			if(!confirm('[TransitionTo] Are you sure you want to visit an About page?')){
+				transition.about(); // Yes = go to this page 'about'
+			}else{ // No = Stay on whatever page user was on.
+				callback();
+			}
+		},
+		// Triggered when users tries to GO AWAY from this page.
+		willTransitionFrom: function (transition,component) {
+			if(!confirm('[TransitionFrom] Sure? Don\'t navigate away from About page.')){
+				transition.about(); // Yes = go to this page 'about'
+			}
+			// No need to call a callback
+		}
+	},
 	render: function(){
 		return (
 			<div>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ var React = require('react');
 var Router= require('react-router');
 var routes = require('./routes');
 
-Router.run(routes,function(Handler){
+// Router.HistoryLocation:
+// 		Removes the # based url. Hashes are ugly. This makes your URL lot cleaner.
+// 		Maintains history ie pushes into browser history stack so you can go back to it.
+Router.run(routes, Router.HistoryLocation, function(Handler){
 	React.render(<Handler />, document.getElementById('app'));
 });
