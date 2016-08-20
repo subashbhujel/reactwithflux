1d95339d193db0f7e431d7143dd75ddecf4b1637
subashbhujel <kathmandu1>
2016-08-20 16:04:51 -0700

    Rerouting to authors list page after form is successfully submitted

--- src/components/author/manageAuthorPage.js
+++ src/components/author/manageAuthorPage.js
@@ -1,11 +1,17 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
 var AuthorForm = require('./authorForm');
+
 // Need a reference to an api for saving the author
 var AuthorApi = require('../../api/authorApi');
 
 var ManageAuthorPage = React.createClass({
+	// Mixins are usually an array. That's it.
+	mixins:[
+		Router.Navigation // Progamatically routing the page
+	],
 	getInitialState: function () {
 		return {
 			author: {id:'', firstName:'',lastName:''}
@@ -28,6 +34,9 @@ var ManageAuthorPage = React.createClass({
 
 		// call apo and save the date
 		AuthorApi.saveAuthor(this.state.author);
+
+		//Saving done. Reroute to authors.
+		this.transitionTo("authors");
 	},
 
 	render: function(){
